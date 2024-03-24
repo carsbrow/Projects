2b02d3105e1cabc3b1371eedc19d3ebb32fd6daa
carsbrow <carson.rbrown5@gmail.com>
2024-03-23 20:18:37 -0500

    Refactor code to improve performance and readability

---
+++ Stock Visualizer/README.md
@@ -0,0 +1,27 @@
+A python script using pandas and matplotlib to visualize the stock data on a graph
+
+Data set is from kaggle, "2019-2024 US Stock Market Data" 
+    https://www.kaggle.com/datasets/saketk511/2019-2024-us-stock-market-data
+
+Data is aviailable for: (From 02/04/2019 - 02/02/2024)
+
+    Meta - Price & Volume
+    Amazon - Price & Volume
+    Netflix - Price & Volume
+    Berkshire - Price & Volume
+    Nvidia - Price & Volume
+    Google - Price & Volume
+    Microsoft - Price & Volume
+    Tesla - Price & Volume
+    Gold - Price & Volume
+    Silver - Price & Volume
+    Apple - Price & Volume
+    Nasdaq_100 - Price & Volume
+    Ethereum - Price & Volume
+    Platinum - Price & Volume
+    Copper - Price & Volume
+    Crude_Oil - Price & Volume
+    Natural_Gas - Price & Volume
+    Bitcoin - Price & Volume
+
+Some fields are missing more data then others so graphs may vary in detail.
---
+++ Stock Visualizer/stock_visualizer.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def stock_visualizer(stock_name, start_date, end_date):
+    """
+    Visualizes the stock data for the given stock name and date range.
+
+    Paramters:
+        stock_name: str
+            The name of the stock to visualize.
+        start_date: str
+            The start date to visualize the stock. (Format: MM-DD-YYYY)
+        end_date: str
+            The end date to visualize the stock. (Format: MM-DD-YYYY)
+        Price: bool
+            If True, visualize the stock price.
+        Volume: bool
+            If True, visualize the stock volume.
+
+    Returns:
+        None
+    """
+
+    stock_df = pd.read_csv("Stock Market Dataset.csv")
+    # Getting the data for the given stock_name
+    stock_df = stock_df.filter(regex=stock_name + '|Date')
+    # Reformating the date to match the date format in the dataset
+    stock_df['Date'] = pd.to_datetime(stock_df['Date'], format='%d-%m-%Y')
+
+    # Filtering the data for the given date range
+    stock_df = stock_df[(stock_df['Date'] >= start_date) & (stock_df['Date'] <= end_date)]
+
+    # Converting the Price and Volume columns to numeric
+    stock_df[stock_name + '_Vol.'] = stock_df[stock_name + '_Vol.'].astype(str)
+    stock_df[stock_name + '_Vol.'] = stock_df[stock_name + '_Vol.'].str.replace(',', '').astype(float)
+
+    stock_df[stock_name + '_Price'] = stock_df[stock_name + '_Price'].astype(str)
+    stock_df[stock_name + '_Price'] = stock_df[stock_name + '_Price'].str.replace(',', '').astype(float)
+    
+    # Replace zeros with NaN
+    stock_df.replace(0, float('nan'), inplace=True)
+    
+    # Grouping by Year-Month with mean values
+    stock_df_grouped = stock_df[[stock_name + '_Price', stock_name + '_Vol.']].groupby([stock_df['Date'].dt.to_period('M')]).mean()
+
+    # Gouping by date
+    stock_df = stock_df[[stock_name + '_Price', stock_name + '_Vol.']].groupby(stock_df['Date']).mean()
+
+    print(stock_df.head())
+
+    # Plotting the data
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+    
+    # Line graph for price
+    stock_df[stock_name + '_Price'].fillna(0).replace(0).plot(kind='line', ax=ax1) 
+    stock_df_grouped[stock_name + '_Price'].fillna(0).replace(0).plot(kind='line', ax=ax1)
+    ax1.set_xlabel('Month')
+    ax1.set_ylabel('Price')
+    ax1.set_title(f'{stock_name} Stock Price by Month')
+    ax1.legend(['Daily Price', 'Monthly Average Price'])
+
+    # Bar graph for monthy volume
+    volume_comparison = stock_df_grouped[stock_name + '_Vol.'].diff()
+    stock_df_grouped[stock_name + '_Vol.'].fillna(0).replace(0).plot(kind='bar', ax=ax2, width=1, edgecolor='black', color=['g' if vol > 0 else 'r' for vol in volume_comparison])
+    ax2.set_xlabel('Month')
+    ax2.set_ylabel('Volume')
+    ax2.set_title(f'{stock_name} Stock Volume by Month')
+    ax2.legend('Monthy Average Volume')
+
+    plt.tight_layout()
+    plt.show()
+    
+
+def main():
+    """Test cases for the stock_visualizer function."""
+    stock_visualizer("Natural_Gas", "04-02-2019", "02-02-2024")
+    stock_visualizer("Gold", "04-02-2019", "02-02-2024")
+    stock_visualizer("Ethereum", "04-02-2019", "02-02-2024")
+    stock_visualizer("Bitcoin", "04-02-2019", "02-02-2024")
+
+
+if __name__ == "__main__":
+    main()
+
