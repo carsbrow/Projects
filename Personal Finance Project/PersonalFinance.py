3ceaaeb2accf8abc7767d7b00d4b106f7b26fb6c
carsbrow <carson.rbrown5@gmail.com>
2024-03-23 21:33:13 -0500

    cleaned up code + README for personal finance

--- Personal Finance Project/PersonalFinance.py
+++ Personal Finance Project/PersonalFinance.py
@@ -1,36 +1,34 @@
-
-
 import gspread
-import TransactionClass
 import re
+from TransactionClass import Transaction
+
 
 
 def spendingData(file_path):
-    """filename as a csv file for transaction data"""
+    """Reads transaction data from a CSV file"""
+    
+    allTransactions = []
     
     with open(file_path, "r") as file:
-
-        allTransancations = []
-        
         for line in file:
-            temp = line[0:-12] #removing unnecessary information
-            data = temp.strip().replace('"','').split(",")
+            temp = line[0:-12]  # Removing unnecessary information
+            data = temp.strip().replace('"', '').split(",")
 
             transacDay = data[0]
-            transacAmmount = float(data[1])
-            transacType = findType(data[4])
-            transacDescription = findDescription(data[4])
-            transacCategory = findCategory(transacAmmount, transacDescription)
+            transacAmount = float(data[1])
+            transacType = find_type(data[4])
+            transacDescription = find_description(data[4])
+            transacCategory = find_category(transacAmount, transacDescription)
             
-            transacObj = TransactionClass.Transaction(transacDay, transacAmmount, transacCategory, transacDescription, transacType)
+            transacObj = Transaction(transacDay, transacAmount, transacCategory, transacDescription, transacType)
             
-            allTransancations.append(transacObj)
+            allTransactions.append(transacObj)
 
-    
-    return allTransancations
+    return allTransactions
 
-def findType(descriptionString):
-    """establishes the type attribute from the banks description string"""
+
+def find_type(descriptionString):
+    """Establishes the type attribute from the bank's description string"""
     
     if "PURCHASE" in descriptionString:
         transacType = "Purchase"
@@ -40,106 +38,75 @@ def findType(descriptionString):
         transacType = "Transfer"
     elif "DEPOSIT" in descriptionString:
         transacType = "Deposit"
+    else:
+        transacType = ""
 
     return transacType
 
-def findDescription(descriptionString):
-    
-    pattern = r'ON \d{1,2}/\d{1,2} (.+?) \w{1}\d{6,}' #pattern, reconizes the date and concludes at rounting number
 
-    match = re.search(pattern, descriptionString) #Searches param for pattern with re
+def find_description(descriptionString):
+    """Extracts the description from the bank's description string"""
+    
+    pattern = r'ON \d{1,2}/\d{1,2} (.+?) \w{1}\d{6,}'  # Pattern recognizes the date and concludes at routing number
+
+    match = re.search(pattern, descriptionString)  # Searches for pattern in the description string
 
     if match:
         description = match.group(1)
-        return description
-    
     elif "TRANSFER" in descriptionString:
-        return "Money Transfer"
-    
+        description = "Money Transfer"
     else:
-        return ""
+        description = ""
 
-def findCategory(ammount, description):
-    """Categories:
-        Income, Food, Subscriptions, Transport & Travel, Tuition, Loans, Other
-    """
+    return description
+
+
+def find_category(amount, description):
+    """Categorizes transactions based on amount and description"""
+    
     lower_descr = description.lower()
     
-    food_keyword = ["mcdon", "taco", "lao", "chick-fil", "chipotle", "starbucks", "coffee", "food", "qdoba", 
-                    "hyve", "target", "walm", "d.p.", "brueg", "walg", "crisp", "noodles", "korean", "nashville", 
-                    "afro deli", "sandwich", "bonchon", "culver"]
+    food_keywords = ["mcdon", "taco", "lao", "chick-fil", "chipotle", "starbucks", "coffee", "food", "qdoba", 
+                     "hyve", "target", "walm", "d.p.", "brueg", "walg", "crisp", "noodles", "korean", "nashville", 
+                     "afro deli", "sandwich", "bonchon", "culver"]
     
-    transport_keyword = ["gas", "train", "metro", "light rail", "transit", "transport"]
+    transport_keywords = ["gas", "train", "metro", "light rail", "transit", "transport"]
 
-    tuition_keyword = ["tuition", "college"]
+    tuition_keywords = ["tuition", "college"]
 
-    loans_keyword = []
+    loans_keywords = []
 
-
-    if ammount > 0:
+    if amount > 0:
         return "Income"
-    
-    elif any(keyword in lower_descr for keyword in food_keyword):
+    elif any(keyword in lower_descr for keyword in food_keywords):
         return "Food"
-    
-    elif any(keyword in lower_descr for keyword in transport_keyword):
+    elif any(keyword in lower_descr for keyword in transport_keywords):
         return "Transport & Travel"
-
-    elif any(keyword in lower_descr for keyword in tuition_keyword):
+    elif any(keyword in lower_descr for keyword in tuition_keywords):
         return "Tuition"
-    
-    elif any(keyword in lower_descr for keyword in loans_keyword):
+    elif any(keyword in lower_descr for keyword in loans_keywords):
         return "Loans"
-
     else:
         return "Other"
 
-def pushData(csvfilepath, month):
 
+def push_data(csv_file_path, month):
+    """Pushes transaction data to a Google Sheets spreadsheet"""
     
     gc = gspread.service_account()
-    
-
     sh = gc.open("Personal Finances")
-    wks = sh.worksheet(f"{month}")
+    wks = sh.worksheet(month)
 
-    transactions = spendingData(csvfilepath)
+    transactions = spendingData(csv_file_path)
 
     row = 8
     for transac in transactions:
-        temp_data = [[transac.getDate(), transac.getAmmount(), transac.getCategory(), transac.getDescription()]]
+        temp_data = [[transac.getDate(), transac.getAmount(), transac.getCategory(), transac.getDescription()]]
         wks.insert_rows(temp_data, row)
 
-        if transac.getCategory() == "Income": #Cell background color green for income
-            wks.format(f"B{row}", 
-                       {"backgroundColor": {
-                           "red": 0.0,
-                           "green": 1.0,
-                           "blue": 0.0
-                       }})
-            
+        if transac.getCategory() == "Income":
+            wks.format(f"B{row}", {"backgroundColor": {"red": 0.0, "green": 1.0, "blue": 0.0}})
         else:
-            wks.format(f"B{row}",  #red for spending
-                       {"backgroundColor": {
-                           "red": 1.0,
-                           "green": 0.0,
-                           "blue": 0.0
-                       }})
+            wks.format(f"B{row}", {"backgroundColor": {"red": 1.0, "green": 0.0, "blue": 0.0}})
+        
         row += 1
-
-    
-    
-    
-    
-
-        
-        
-    
-
-
-    
-
-
-
-
-
